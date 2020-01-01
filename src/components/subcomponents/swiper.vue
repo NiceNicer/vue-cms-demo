816056fb5334393135d8fb628173030bb34f3e72
NiceNicer <hengge2012@sina.com>
2020-01-01 16:49:36 +0800

    cart

---
+++ src/components/subcomponents/swiper.vue
@@ -0,0 +1,45 @@
+<temnplate>
+    <div>
+        <mt-swipe :auto="4000">
+            <!--父组件向子组件传lunbotuList-->
+            <mt-swipe-item v-for="item in lunbotuList" :key="item.id">
+
+                <img :src="item.img_url" alt="">
+            </mt-swipe-item>
+
+
+        </mt-swipe>
+
+    </div>
+
+</temnplate>
+
+<script type="text/ecmascript-6">
+    export default {
+        props: ["lunbotuList"]
+    }
+
+</script>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+    .mint-swipe{
+        height: 200px;
+
+        .mint-swipe-item{
+            &:nth-child(1){
+                background-color: red;
+            }
+            &:nth-child(2){
+                background-color: blue;
+            }
+            &:nth-child(3){
+                background-color: yellow;
+            }
+            img{
+                width:100%;
+                height: 100%;
+            }
+        }
+    }
+
+</style>
